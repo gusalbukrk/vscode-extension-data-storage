900dd53da6dffe271b71c016cd8f24696c2eb39f
gusalbukrk <gusalbukrk@gmail.com>
2024-02-01 12:04:12 -0300

    `context.storageUri`, `context.globalStorageUri`

--- src/extension.ts
+++ src/extension.ts
@@ -55,10 +55,58 @@ export function activate(context: vscode.ExtensionContext) {
 		// storage location varies by OS
 		// https://github.com/microsoft/vscode-discussions/discussions/748#discussioncomment-7741629
 		// available methods are: get, store and delete (all of them have a thenable return type)
-		await context.secrets.store('my_secret', 'foobar');
-		console.log(await context.secrets.get('my_secret'));
-		await context.secrets.delete('my_secret');
-		console.log(await context.secrets.get('my_secret'));
+		// await context.secrets.store('my_secret', 'foobar');
+		// console.log(await context.secrets.get('my_secret'));
+		// await context.secrets.delete('my_secret');
+		// console.log(await context.secrets.get('my_secret'));
+
+		// gets uri of a workspace specific directory in which the extension can store private state
+		// the directory might not exist and creation is up to the extension (the parent directory is guaranteed to be existent)
+		// e.g. `/home/username/.config/Code/User/workspaceStorage/131b9543b3558969270727be3be682e5/publisher.extension`
+		// console.log(context.storageUri);
+
+		// if (!context.storageUri) {
+		// 	// storageUri is undefined when there is no workspace (no folder opened)
+		// 	return;
+		// }
+
+		// // create the extension's workspace storage folder if it doesn't already exist
+		// try {
+		// 		// when folder doesn't exist, and error gets thrown
+		// 		await vscode.workspace.fs.stat(context.storageUri);
+		// } catch {
+		// 		// create the extension's workspace storage folder
+		// 		console.log('creating directory...');
+		// 		await vscode.workspace.fs.createDirectory(context.storageUri)
+		// }
+
+		// const workspaceData = vscode.Uri.joinPath(context.storageUri, 'my-storage-file.json');
+		// const writeData = new TextEncoder().encode(JSON.stringify({ now: Date.now() }));
+		// vscode.workspace.fs.writeFile(workspaceData, writeData);
+
+		// gets uri of a workspace specific directory in which the extension can store private state
+		// the directory might not exist and creation is up to the extension (the parent directory is guaranteed to be existent)
+		// e.g. `/home/username/.config/Code/User/globalStorage/publisher.extension/`
+		console.log(context.globalStorageUri);
+
+		if (!context.globalStorageUri) {
+			console.log('!!!');
+			return;
+		}
+
+		// create the extension's workspace storage folder if it doesn't already exist
+		try {
+				// when folder doesn't exist, and error gets thrown
+				await vscode.workspace.fs.stat(context.globalStorageUri);
+		} catch {
+				// create the extension's workspace storage folder
+				console.log('creating directory...');
+				await vscode.workspace.fs.createDirectory(context.globalStorageUri)
+		}
+
+		const workspaceData = vscode.Uri.joinPath(context.globalStorageUri, 'my-storage-file.json');
+		const writeData = new TextEncoder().encode(JSON.stringify({ now: Date.now() }));
+		vscode.workspace.fs.writeFile(workspaceData, writeData);
 	});
 
 	context.subscriptions.push(disposable);
